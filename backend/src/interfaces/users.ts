3532a2961510dd70066ada8fa3863db7430c6edb
PawanKumar Shukla <132829129+pawanshukla0963@users.noreply.github.com>
2023-12-05 20:41:49 +0530

    Delete backend directory

--- backend/src/interfaces/users.ts
+++
@@ -1,8 +0,0 @@
-export default interface IUser {
-    id: number;
-    email: string;
-	full_name: string;
-	phone_number: string
-    password: string;
-	user_type: string;
-}
--- backend/src/routes/employee.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import controller from '../controllers/employee';
-
-const router = express.Router();
-
-router.get('/get_tasks/:id', controller.getTasks);
-
-export default router;
